3a3ae1cae5644ba9325634bba98d5cdd9ee93a40
NiceMan1337 <37738748+NiceMan1337@users.noreply.github.com>
2022-10-16 13:43:11 +0200

    Upload

---
+++ 01/1.1.c
@@ -0,0 +1,15 @@
+/*  Name: 1.1.c
+    Author: Ted Jensen 
+    Date: 16.10.2022  */
+
+#include <stdio.h>
+int main()
+{
+    printf("size of a short is %d\n", sizeof(short));
+
+    printf("size of a int is %d\n", sizeof(int));
+
+    printf("size of a long is %d\n", sizeof(long));
+
+    return 0;
+}

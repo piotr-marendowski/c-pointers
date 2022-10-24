1716dd3c9c43510ce2e4c07001f941b013a406dc
Piotr Marendowski <37738748+piotr-marendowski@users.noreply.github.com>
2022-10-24 18:26:20 +0200

    Add files via upload

---
+++ 10/10.2.c
@@ -0,0 +1,50 @@
+/*  Name: 10.2.c
+    Author: Ted Jensen 
+    Date: 24.10.2022  */
+
+#include <stdio.h>
+
+int arr[10] = {3,6,1,2,3,8,4,1,7,2};
+
+void bubble(int a[], int N);
+int compare(int m, int n);
+
+int main()
+{
+    int i;
+    putchar('\n');
+    for (i = 0; i < 10; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    bubble(arr,10);
+    putchar('\n');
+
+    for (i = 0; i < 10; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
+
+void bubble(int a[], int N)
+{
+    int i, j, t;
+    for (i = N-1; i >= 0; i--)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            if (compare(a[j-1], a[j]))
+            {
+                t = a[j-1];
+                a[j-1] = a[j];
+                a[j] = t;
+            }
+        }
+    }
+}
+
+int compare(int m, int n)
+{
+    return (m > n);
+}
